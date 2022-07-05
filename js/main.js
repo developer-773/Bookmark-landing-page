73ef8915e5e89caf0567785b0ac73d6b92d31b81
Muhammadbek773 <galaxylogin@yandex.ru>
2022-07-05 18:24:30 +0500

    Added js file

--- js/main.js
+++ js/main.js
@@ -1,5 +1,31 @@
-const elChangeThemeBtn = document.querySelector("#faq-wrap__summary");
+//LI va href elementlarinig batchasini DOM dan js file ga chaqirib olamiz
+var elItem = document.querySelectorAll(".features-list__item");
+var elLink = document.querySelectorAll(".features-list__link");
 
-elChangeThemeBtn.addEventListener("click" , function(){
-    elChangeThemeBtn.classList.toggle("sv");
-});
+var elFeaturesItem = document.querySelectorAll(".info-wrap");
+//LiNK larni barchasiga birdak tasir qilish uchun sikl (forEach) bilan aylanishi.
+
+elLink.forEach(function(links) {
+    // linklarni hammasini eshitib turuvchi hodisa va link bosilgan payt qandaydir hodisani amalga oshirish
+    links.addEventListener("click", function(evt) {
+        // link sakrashi ya'ni refresh bo'lishini taqiqlash
+        evt.preventDefault();
+        //ITEM larni barchasiga birdak tasir qilish uchun sikl (forEach) bilan aylanishi.
+        
+        elItem.forEach(function(item){
+            // item elementidagi classlar ro'yxatidan shu classni o'chirish
+            item.classList.remove("features-list__item--mod");
+        })
+        // item elementidagi classlar ro'yxatidan shu classni o'chirish
+        links.parentElement.classList.add("features-list__item--mod");
+
+        elFeaturesItem.forEach(function(box){
+            box.classList.remove("info-wrap__txt--mod");
+
+        })
+
+        document.querySelector(links.getAttribute("href")).classList.add("info-wrap__txt--mod")
+
+    })
+        
+    })
